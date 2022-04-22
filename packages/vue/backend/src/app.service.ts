26ea9cc0d6b321b31075e307185800135f83c907
Seonkyu Kim <somedaycode@gmail.com>
2022-04-22 20:06:45 +0900

    feat: backend SSR을 위한 service 및 controller 수정

--- packages/vue/backend/src/app.service.ts
+++ packages/vue/backend/src/app.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { BundleRenderer, createBundleRenderer } from 'vue-server-renderer';
+import { join } from 'path';
+
+const RESOURCES_PATH = join(process.cwd(), '../resources');
+const SERVER_BUNDLE_PATH = join(
+  process.cwd(),
+  '../resources/vue-ssr-server-bundle',
+);
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public async getSSRHTML(): Promise<BundleRenderer> {
+    const template = readFileSync(
+      join(RESOURCES_PATH, '/index-ssr.html'),
+      'utf-8',
+    );
+
+    const ssrBundle = await import(SERVER_BUNDLE_PATH);
+    const renderer = createBundleRenderer(ssrBundle, {
+      template,
+    });
+
+    return renderer;
   }
 }
